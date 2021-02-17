4403bed2e631b0d8c4b87f2b4f60aa1c3cfa23ae
Jasonhadzi <jasonhadzi@gmail.com>
2021-02-17 18:18:51 +0200

    adds new files

---
+++ doubly_linked_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 15 19:24:00 2021
+
+@author: jasonhadzikostas
+"""
+
+class Node:
+  def __init__(self, value, next_node=None, prev_node=None):
+    self.value = value
+    self.next_node = next_node
+    self.prev_node = prev_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
+
+  def set_prev_node(self, prev_node):
+    self.prev_node = prev_node
+    
+  def get_prev_node(self):
+    return self.prev_node
+  
+  def get_value(self):
+    return self.value
+    
+
+class DoublyLinkedList:
+  pass
